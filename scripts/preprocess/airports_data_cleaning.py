ae10e166f9ca9231fa95c20c93dca05b9e903e4a
S-col-env <silvia.colombo@ouce.ox.ac.uk>
2024-12-02 12:25:54 +0000

    new script for airports and multimodal newtworks and new ultils script added

---
+++ scripts/preprocess/airports_data_cleaning.py
@@ -0,0 +1,145 @@
+import sys
+import os
+import re
+import pandas as pd
+import geopandas as gpd
+import igraph as ig
+from shapely.geometry import Point
+from math import radians, cos, sin, asin, sqrt
+from haversine import haversine
+from utils_new import *
+from tqdm import tqdm
+import country_converter as coco
+
+tqdm.pandas()
+
+def add_iso_code(df,df_id_column,incoming_data_path):
+    # Insert countries' ISO CODE
+    africa_boundaries = gpd.read_file(os.path.join(
+                            incoming_data_path,
+                            "Africa_GIS Supporting Data",
+                            "a. Africa_GIS Shapefiles",
+                            "AFR_Political_ADM0_Boundaries.shp",
+                            "AFR_Political_ADM0_Boundaries.shp"))
+    africa_boundaries.rename(columns={"DsgAttr03":"iso3"},inplace=True)
+    # Spatial join
+    m = gpd.sjoin(df, 
+                    africa_boundaries[['geometry', 'iso3']], 
+                    how="left", predicate='within').reset_index()
+    m = m[~m["iso3"].isna()]        
+    un = df[~df[df_id_column].isin(m[df_id_column].values.tolist())]
+    un = gpd.sjoin_nearest(un,
+                            africa_boundaries[['geometry', 'iso3']], 
+                            how="left").reset_index()
+    m = pd.concat([m,un],axis=0,ignore_index=True)
+    return m
+
+def main(config):
+    incoming_data_path = config['paths']['incoming_data']
+    processed_data_path = config['paths']['data']
+    
+    epsg_meters = 3395 # To convert geometries to measure distances in meters
+    cutoff_distance = 6600 # We assume airports within 6.6km are the same
+
+    df_airports_flow = gpd.read_file(os.path.join(incoming_data_path,
+                                    "airports",
+                                    "airport_flows.shp"))
+    
+    df_airports_nodes = gpd.read_file(os.path.join(incoming_data_path,
+                                    "airports",
+                                    "airport_volume_airport_location.gpkg"))
+    
+    # This contains geometry is wrong, which we will have to convert to Point from Latitude and Longitude values
+    
+    df_airports_flow= df_airports_flow.to_crs(epsg=4326)
+
+    df_airports_flow= add_iso_code(df_airports_flow,"Country1",incoming_data_path)
+    df_airports_flow.rename(columns={"iso3": "iso3_1"},inplace=True)  
+    df_airports_flow.drop(columns=["index_right"], inplace = True)  
+    
+    df_airports_flow = add_iso_code(df_airports_flow,"Country2",incoming_data_path)
+    df_airports_flow.rename(columns={"iso3": "iso3_2"},inplace=True) 
+    
+    df_airports_flow.drop(columns = ["level_0"], inplace=True)
+
+    cc = coco.CountryConverter()
+    df_airports_flow["continent1"]  = cc.pandas_convert(df_airports_flow["Country1"], to='Continent')  
+    df_airports_flow["continent2"]  = cc.pandas_convert(df_airports_flow["Country2"], to='Continent')  
+
+    print(df_airports_flow)
+    
+
+    df_airports_nodes["geom"] = gpd.points_from_xy(
+                            df_airports_nodes["Airport1Longitude"],df_airports_nodes["Airport1Latitude"])
+    df_airports_nodes.drop("geometry",axis=1,inplace=True)
+    df_airports_nodes.rename(columns={"geom":"geometry"},inplace=True)
+
+    df_airports_nodes = gpd.GeoDataFrame(df_airports_nodes,geometry="geometry",crs="EPSG:4326")
+
+    df_airports_nodes= df_airports_nodes.to_crs(epsg=4326)
+    
+    df_airports_nodes = add_iso_code(df_airports_nodes,"Country Name",incoming_data_path)
+
+    df_airports_nodes["infra"] = "airport"
+
+    df_airports_nodes["continent"]  = cc.pandas_convert(df_airports_nodes["Country Name"], to='Continent')  
+
+
+    print(df_airports_nodes)
+
+     
+    network = create_network_from_nodes_and_edges(df_airports_nodes.to_crs(epsg=epsg_meters), 
+                                                  df_airports_flow.to_crs(epsg=epsg_meters),
+                                                  "airports",
+                                snap_distance=6000,
+                                geometry_precision=True)
+
+    edges = network.edges.set_crs(epsg=epsg_meters)
+    nodes = network.nodes.set_crs(epsg=epsg_meters)
+    edges, nodes = components(edges,nodes,
+                                node_id_column="node_id",
+                                edge_id_column="edge_id",
+                                from_node_column="from_node",
+                                to_node_column="to_node")
+    nodes.to_file(os.path.join(
+                            processed_data_path,
+                            "infrastructure",
+                            "global_airport_network.gpkg"),
+                        layer="nodes",driver="GPKG")
+    edges.to_file(os.path.join(processed_data_path,
+                            "infrastructure",
+                            "global_airport_network.gpkg"),
+                        layer="edges",driver="GPKG")
+    
+    
+    
+    # Get the airports for Africa
+
+
+    airport_edges = gpd.read_file(os.path.join(processed_data_path,
+                            "infrastructure",
+                            "global_airport_network.gpkg"),layer="edges")
+    airport_nodes = gpd.read_file(os.path.join(processed_data_path,
+                            "infrastructure",
+                            "global_airport_network.gpkg"),layer="nodes")
+   
+    africa_airports = airport_nodes[airport_nodes["continent"] == "Africa"]
+   
+    africa_air_tracks = airport_edges[airport_edges["continent1" or "continent2"] == "Africa"]
+    
+    print(africa_airports)
+    
+    africa_airports.to_file(os.path.join(
+                            processed_data_path,
+                            "infrastructure",
+                            "africa_airport_network.gpkg"),
+                        layer="nodes",driver="GPKG")
+    africa_air_tracks.to_file(os.path.join(processed_data_path,
+                            "infrastructure",
+                            "africa_airport_network.gpkg"),
+                        layer="edges",driver="GPKG")
+
+
+if __name__ == '__main__':
+    CONFIG = load_config()
+    main(CONFIG)
---
+++ scripts/preprocess/multi_modal_edges_creation.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import LineString
+from utils_new import *
+from tqdm import tqdm
+tqdm.pandas()
+
+config = load_config()
+incoming_data_path = config['paths']['incoming_data']
+processed_data_path = config['paths']['data']
+
+
+def get_mode_dataframe(mode,rail_status=["open"],rail_to_road_connection=False):
+    if mode == "air":
+        nodes =  gpd.read_file(os.path.join(
+                                processed_data_path,
+                                "infrastructure",
+                                "africa_airport_network.gpkg"
+                                    ), layer="nodes"
+                                )  
+        nodes = nodes[nodes["infra"] == "airport"]   
+    elif mode == "sea":
+        nodes =  gpd.read_file(os.path.join(
+                                processed_data_path,
+                                "infrastructure",
+                                "africa_maritime_network.gpkg"
+                                    ), layer="nodes"
+                                )  
+        nodes = nodes[nodes["infra"] == "port"]
+    elif mode == "IWW":
+        nodes = gpd.read_file(os.path.join(
+                                processed_data_path,
+                                "infrastructure",
+                                "africa_iww_network.gpkg"
+                                    ), layer="nodes"
+                                ) 
+        nodes = nodes[nodes["infra"] == "IWW port"]
+    elif mode == "rail":
+        rail_edges = gpd.read_file(os.path.join(
+                                processed_data_path,
+                                "infrastructure",
+                                "africa_railways_network.gpkg"
+                                    ), layer="edges"
+                        )
+        rail_edges = rail_edges[rail_edges["status"].isin(rail_status)]
+        rail_node_ids = list(set(rail_edges["from_id"].values.tolist() + rail_edges["to_id"].values.tolist()))
+        nodes = gpd.read_file(os.path.join(
+                                processed_data_path,
+                                "infrastructure",
+                                "africa_railways_network.gpkg"
+                                    ), layer="nodes"
+                        )
+        nodes = nodes[(nodes["id"].isin(rail_node_ids)) & (nodes["infra"].isin(['stop','station']))]
+
+        degree_df = rail_edges[["from_id","to_id"]].stack().value_counts().rename_axis('id').reset_index(name='degree')
+        nodes = pd.merge(nodes,degree_df,how="left",on=["id"])
+
+        if rail_to_road_connection is True:
+            freight_facility_types = ["port","port (dry)",
+                                        "port (inland)",
+                                        "port (river)",
+                                        "road-rail transfer",
+                                        "container terminal",
+                                        "freight terminal",
+                                        "freight marshalling yard",
+                                        "mining",
+                                        "refinery"]
+            nodes = nodes[
+                                            (
+                                                nodes["facility"].isin(freight_facility_types)
+                                            ) | (
+                                                nodes["degree"] == 1
+                                            )
+                                        ]
+    elif mode == "road": 
+        nodes = gpd.read_parquet(os.path.join(
+                                processed_data_path,
+                                "infrastructure",
+                                "africa_roads_nodes.geoparquet"))
+        nodes.rename(columns={"iso_a3":"iso3"},inplace=True)
+
+    return nodes
+
+def main():
+    epsg_meters = 3395 # To convert geometries to measure distances in meters
+    from_modes = ["sea","sea","IWW","IWW","rail", "air","air"]
+    to_modes = ["rail","road","rail","road","road","rail","road"]
+
+    multi_df = []
+    for idx,(f_m,t_m) in enumerate(zip(from_modes,to_modes)):
+        if f_m == "rail" and t_m == "road":
+            f_df = get_mode_dataframe(f_m,rail_to_road_connection=True)
+        else:
+            f_df = get_mode_dataframe(f_m)
+
+        t_df = get_mode_dataframe(t_m)
+        f_t_df = create_edges_from_nearest_node_joins(
+                            f_df.to_crs(epsg=epsg_meters),
+                            t_df.to_crs(epsg=epsg_meters),
+                            "id","id",
+                            "iso3","iso3",
+                            f_m,t_m)
+        if len(f_t_df) > 0:
+            multi_df.append(f_t_df)
+            c_t_df = f_t_df[["from_id","to_id","from_infra",
+                        "to_infra","from_iso_a3","to_iso_a3",
+                        "link_type","length_m","geometry"]].copy()
+            c_t_df.columns = ["to_id","from_id",
+                        "to_infra","from_infra",
+                        "to_iso_a3",
+                        "from_iso_a3",
+                        "link_type","length_m","geometry"]
+            multi_df.append(c_t_df)
+
+    multi_df = gpd.GeoDataFrame(
+                    pd.concat(multi_df,axis=0,ignore_index=True),
+                    geometry="geometry",crs=f"EPSG:{epsg_meters}")
+    multi_df = multi_df.to_crs(epsg=4326)
+    multi_df["id"] = multi_df.index.values.tolist()
+    multi_df["id"] = multi_df.progress_apply(lambda x:f"intermodale_{x.id}",axis=1)
+    
+    
+    multi_df.to_file(os.path.join(
+                            processed_data_path,
+                            "infrastructure",
+                            "africa_multimodal.gpkg"
+                                ), 
+                            layer="edges",
+                            driver="GPKG"
+                            )
+
+if __name__ == '__main__':
+    main()
