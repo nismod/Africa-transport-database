98266b680d59b014817648964074344bf8fbacc1
S-col-env <silvia.colombo@ouce.ox.ac.uk>
2025-01-22 11:32:06 +0000

    updated corridors script

--- scripts/preprocess/road_corridors.py
+++ scripts/preprocess/road_corridors.py
@@ -22,24 +22,29 @@ def main(config):
     # Read the road edges data for Africa
 
     road_id_column = "id"
+    node_id_column = "id"
     road_type_column = "tag_highway"
     main_road_types = ["trunk","motorway","primary","secondary"]
     # """
     # Assuming that the starting point is the road network with the main highways
     # Because main corridors should be along main highways
     # """
-    road_edges = gpd.read_parquet(os.path.join(incoming_data_path,
+    
+    road_edges = gpd.read_parquet(os.path.join(
+                            incoming_data_path,
                             "africa_roads",
                             "edges_with_topology.geoparquet"))
-    road_nodes = gpd.read_parquet(os.path.join(incoming_data_path,
+    road_nodes = gpd.read_parquet(os.path.join(
+                            incoming_data_path,
                             "africa_roads",
                             "nodes_with_topology.geoparquet"))
     road_edges = road_edges.to_crs(epsg=epsg_meters)
     road_nodes = road_nodes.to_crs(epsg=epsg_meters) 
-    main_roads = road_edges[
-                        road_edges[road_type_column].isin(main_road_types)
-                        ][road_id_column]
-
+    #main_edges = gpd.read_parquet(os.path.join(
+                                # processed_data_path,
+                                # "infrastructure",
+                                # "africa_roads_edges.geoparquet"))
+    
     # """
     # Read the file with the start and end points of the corridor
     # Example data: (can include more column names with values, but these are the minimum)
@@ -117,39 +122,31 @@ def main(config):
 
         corridor_df2.drop(["geometry","index_right"],axis=1,inplace=True)
 
-        print(corridor_df2)
+    print(corridor_df2)
          
     corridor_df = corridor_df.join(corridor_df2, lsuffix='_start', rsuffix='_end')
+    print("corridor_df")
+    print(corridor_df)
+
     corridor_df.drop(["index_start","index_end","corridor_name_start"],axis=1,inplace=True)
     corridor_df.rename(columns={"corridor_name_end":"corridor_name"}, inplace=True)
-     # corridor_df = pd.concat([corridor_df, corridor_df2]axis=0)
-    corridor_df = corridor_df.reindex(columns=['start_location','source','end_location','target','corridor_name'])
+    # corridor_df = pd.concat([corridor_df, corridor_df2]axis=0)
+    # corridor_df = corridor_df.reindex(columns=['start_location','source','end_location','target','corridor_name'])
 
-    print(corridor_df) 
-    
+
     graph = create_igraph_from_dataframe(
                     road_edges[["from_id","to_id",road_id_column,"length_m"]])
-
+    
+    
     roads_with_corridors = []
     corridor_name_column = "corridor_name"
-    
-    print(corridor_df)
-    
-   
 
     for row in corridor_df.itertuples():            
-        path = graph.get_shortest_paths(
         path = graph.get_shortest_paths(
                                         row.source, 
                                         row.target, 
                                         weights="length_m",output="epath")[0]
-        
-        connected_roads = []
-        if path:
-            for n in path:
-                connected_roads.append(graph.es[n]["id"])
-        corridor_names = [getattr(row,corridor_name_column)]*len(connected_roads)
-                                        weights="length_m", output="epath")[0]
+
         connected_roads = []
         if path:
             for n in path:
@@ -157,19 +154,11 @@ def main(config):
         corridor_names = [getattr(row,corridor_name_column)]*len(connected_roads)
         roads_with_corridors += list(zip(connected_roads,corridor_names))
 
-        
-           
-
     roads_with_corridors = pd.DataFrame(roads_with_corridors,columns=["id","corridor_name"])
-    # roads_with_corridors["new_id"] = roads_with_corridors.progress_apply(lambda x:f"edge_{x.id}",axis=1)
+    
 
-    
-    
-    
-    # roads_with_corridors.drop(["id"],axis=1,inplace=True)
-    # roads_with_corridors.rename(columns={"new_id":"id"},inplace=True)
-    
-    
+                                 
+
     # Could be possible that a road might have multiple corridors
     roads_with_corridors = roads_with_corridors.groupby("id").agg(
                                                             {
@@ -178,38 +167,64 @@ def main(config):
                                                             ).reset_index()
     
     
-    
+
     # This will covert values from a list to a string seperated by / 
     # Example: ['a','b','c'] becomes 'a/b/c' 
     roads_with_corridors["corridor_name"] = roads_with_corridors["corridor_name"].progress_apply(lambda a: "/".join(list(set(a))))
+                         
     
-   
+    road_edges = pd.merge(road_edges,roads_with_corridors,how="left",on=["id"])
+    
+    connected_nodes = list(set(road_edges.from_id.values.tolist() + road_edges.to_id.values.tolist()))
+    nearest_nodes = road_nodes[road_nodes[node_id_column].isin(connected_nodes)]
+    nearest_nodes.rename(columns={node_id_column:"id"},inplace=True)
+    nearest_nodes = nearest_nodes.to_crs(epsg=4326)
 
-    # roads_with_corridors = list(set(roads_with_corridors + main_roads))
-    # roads_with_corridors = road_edges[
-    #                     road_edges[road_id_column].isin(roads_with_corridors)
-    #                     ]
-    # roads_with_corridors = roads_with_corridors.to_crs(epsg=4326)
-    # connected_nodes = list(set(roads_with_corridors.from_id.values.tolist() + roads_with_corridors.to_id.values.tolist()))
-    # roads_with_corridors = road_nodes[road_nodes[node_id_column].isin(connected_nodes)]
-    # roads_with_corridors.rename(columns={node_id_column:"id"},inplace=True)
-    # roads_with_corridors = roads_with_corridors.to_crs(epsg=4326)
-    
-   
-    road_edges = pd.merge(main_roads,roads_with_corridors,on="id",how="left")
-    
+    """Find the network components
+    """
+    edges = road_edges[[
+            'from_id','to_id','id','osm_way_id','from_iso_a3','to_iso_a3',
+            'tag_highway', 'tag_surface','tag_bridge','tag_maxspeed','tag_lanes',
+            'bridge','paved','material','lanes','width_m','length_m','asset_type','corridor_name','geometry']]
+    edges, nearest_nodes = components(edges,nearest_nodes,node_id_column="id")
+
+    edges["border_road"] = np.where(edges["from_iso_a3"] == edges["to_iso_a3"],0,1)
+
+    nearest_nodes = gpd.GeoDataFrame(nearest_nodes,
+                    geometry="geometry",
+                    crs="EPSG:4326")
+
+    edges = gpd.GeoDataFrame(edges,
+                    geometry="geometry",
+                    crs="EPSG:4326")
+
+    nearest_nodes.to_parquet(os.path.join(
+                            processed_data_path,
+                            "infrastructure",
+                            "africa_roads_nodes_PROVA.geoparquet"))
+    edges.to_parquet(os.path.join(
+                            processed_data_path,
+                            "infrastructure",
+                            "africa_roads_edges_PROVA.geoparquet"))
    
     
     # Write  the file back to the original if you are feeling confident!
     # Otherwise rename
-    road_edges.to_parquet(
-                            os.path.join(
-                                    processed_data_path,
-                                    "infrastructure",
-                                    "africa_roads_edges_TRY.geoparquet"
-                                    )
-                        )
-    
+    # roads_with_corridors.to_csv(
+    #                         os.path.join(
+    #                                 processed_data_path,
+    #                                 "infrastructure",
+    #                                 "roads_corridors_PROVA.csv"
+    #                                 )
+    #                     )
+    # road_edges.to_parquet(
+    #                         os.path.join(
+    #                                 processed_data_path,
+    #                                 "infrastructure",
+    #                                 "africa_roads_edges_PROVA.geoparquet"
+    #                                 )
+    #                     )
+   
 
 
 
